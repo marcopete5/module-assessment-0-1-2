9a9de096a32b2ab5b1c9c4f2bafc03aeac577742
Marcus Peterson <mdabomb5@gmail.com>
2019-10-28 13:08:25 -0600

    added some more

---
+++ destructuring/app.js
@@ -0,0 +1,8 @@
+// Destructure Name and isAlive out of the person object
+
+const person = {
+    name: 'Roger',
+    age: 23,
+    hobbies: ['food fighting', 'internet trolling'],
+    isAlive: false
+}
---
+++ objects/app.js
@@ -0,0 +1,24 @@
+const person = {
+    name: 'Rupert',
+    age: 34,
+    hobbies: [
+        {
+            name: 'Skiing',
+            skillLevel: 'Expert',
+            favPlaces: ['Colorado', 'Utah', 'Canada']
+        },{
+            name: 'Kayaking',
+            skillLevel: 'Professional',
+            favPlaces: ['Idaho', 'Montana']
+        },{
+            name: 'Mountain Biking',
+            skillLevel: 'Novice',
+            favPlaces: ['St.George', 'Corner Canyon', 'LCC']
+        }
+    ],
+    hasHair: false
+}
+
+// How would you access age?
+// How would you access 'Kayaking'?
+// How would you access 'LCC' ?
